a636a511b980c371249556cc54f9cee199a36e3d
Hema-01-hub <hemakrishnasree777@gmail.com>
2026-05-30 19:14:17 +0530

    Added Vercel URL to CORS

--- server/main.js
+++ server/main.js
@@ -10,7 +10,7 @@ const app = express();
 // app.use(cors());
 // app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:3001", "http://localhost:3002"],
+  origin: ["http://localhost:3000", "http://localhost:3001", "http://localhost:3002","https://full-stack-blog-app-beta.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
